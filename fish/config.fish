0dcabec5ce6c8516923da5f7a3ccf0d626d63609
Tyler <twebster137@gmail.com>
2019-07-19 23:40:10 -0500

    lower terminal transparency, switch to google dark theme

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,11 @@ set_abbr
 set_aliases
 set_exports
 
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
+
 fish_vi_key_bindings
 
