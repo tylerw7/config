64af6d335ad3b04a0456d7af59b2b44f80f7ae61
Tyler Webster <twebster137@gmail.com>
2020-02-18 23:54:12 -0600

    Add monitor autodetection in polybar launch script, add bar transparency, remove border and put i3 module in the center. Add mute toggle, and set i3lock background to ~/Pictures/Background/.lock symlink in sxhkdrc.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,8 +2,17 @@
 
 killall -q polybar
 
+# Get connected MONITORS (1 and 2) env vars, TODO: Make this faster
+# . ~/.scripts/monitors.sh
+
+# This also takes too long (slightly better?)
+MONITORS=$(polybar -m)
+export MONITOR1=$(echo "$MONITORS" | grep "DP" | sed -e 's/:.*$//g')
+export MONITOR2=$(echo "$MONITORS" | grep "HDMI" | sed -e 's/:.*$//g')
+
 echo "---" | tee -a /tmp/bar1.log /tmp/bar2.log
 polybar bar1 >> /tmp/bar1.log 2>&1 &
 polybar bar2 >> /tmp/bar2.log 2>&1 &
 
 echo "Bars launched..."
+
